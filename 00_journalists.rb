0c26978dd10c11300ce59c7d9d0de2ef7ebaa08b
Julie K <juliette.nada@gmail.com>
2019-04-05 16:42:08 +0200

    journaliste done

--- 00_journalists.rb
+++ 00_journalists.rb
@@ -1,34 +1,95 @@
 $list = ["@jcunniet","@PaulLampon","@Aziliz31","@ssoumier","@marionsouzeau","@gaellombart","@bendarag","@AurelieLebelle","@julienduffe","@thomaspoupeau","@LilyRossignol","@ClairGuedon","@stephanieauguy","@claw_prolongeau","@_JulieMenard","@LColcomb","@Zlauwereys","@MeLonguet","@DorotheeLN","@NolwennCosson","@ADaboval","@Remibaldy","@bderveaux","@amandechap","@ELODIESOULIE","@nbongarcon","@HeloAb","@Panamorama","@gregplou","@BenoitBerthe","@LauraBruneau89","@Anthony_Lieures","@Sharonwaj","@mcsonkin","@pverduzier","@emiliel3","@Julien_MARION","@SophiFay","@bdelombre","@annalecerf","@AdriaBudry","@DejNikolic","@iJaffre","@CyrusleVirus","@GPardigon","@e_vallerey","@IsabelleSouquet","@AudeDavidRossi","@Yoann_Pa","@CeliaPenavaire","@perraultvincent","@cboulate","@JustineWeyl","@Paulinejacot","@juliechab","@aslechevallier","@phnou","@Seb_Pommier","@Alex_Bensaid","@GuillaumeGaven","@annelaurechouin","@Oliviader","@guerricp","@JMMarchaut","@cyceron","@gregory_raymond","@vhunsinger","@l_peillon","@fannyguinochet","@EAssayag","@KibweAdom","@YvonHerry","@JohanGarciajg","@saidlabidi","@lauranneprov","@LeaDavy","@francois_remy","@CGuicheteau","@FloMaillet","@m_perroud","@oBrunet_TSMF","@MoonVdc","@jc2taille","@NellyMoussu","@VirginK","@b_misa","@FabriceCouste","@barbara_prose","@lelia2m","@brunoaskenazi","@laurenechamp","@ysisbox","@juliengagliardi","@PierreLel","@kdeniau","@_TerraInc","@DominicArpin","@antoinfonteneau","@nanotousch","@jb_roch","@YaniKhezzar","@Anne_Bechet","@NCapart","@SamyBenNaceur","@Joumany","@Julietteraynal","@TGiraudet","@SaraTanit","@HappeFrederic","@antoinellorca","@michelpicot","@Sev_Ryne","@bobdobolino","@murdever","@YGrandmontagne","@Mnyo","@EdKOSCIANSKI","@tnoisette","@jankari","@delbello_rom","@rflechaux","@NadiaSorelli","@IT_Digital","@abarbaux","@PhilippeLeroy","@schaptal","@marionspee","@lisavignoli","@ChloeAeberhardt","@MartineJacot","@JuliaPascualita","@curieusedetout","@sgraveleau","@bif_o","@ElisaPineau","@zinebdryef","@apiquard","@pierrehaski","@StephanieDelmas","@Blandine_Garot","@vergara_i","@evan_lebastard","@SophieVclt","@OlivierLevrault","@alicedorgeval","@LouiseMalnoy","@alix_fx","@pierre_baudis","@LucMagoutier","@AgatheMuller","@SGianninelli","@PaulineBoyer33","@NaomiHalll","@romaindlx","@marionbr","@Burtschy","@JacobEtienne","@as_lizzani","@marie_simon","@LaureDaussy","@FabriceAmedeo","@LoubnaChlaikhy","@PlummerWilliam","@OlivierMarin1","@alaurefremont","@mwesfreid","@ChBaDe","@pmathon","@theobaldmarie","@Lnpagesy","@marclandre","@paoliniesther","@Feertchak","@JBLitzler","@GuillaumeErrard","@quentinperinel","@TristanQM","@mlbo","@constancejamet","@LoraTerrazas","@emiliegeffray","@Mathilde_Sd","@CaroPiquet","@DCanivez","@TIM_7375","@blandinelc","@ivanrioufol","@arthurberdah","@SarahLecoeuvre","@guillaume_gui","@DamienMercereau","@W_Chloe","@Assma_MD","@EugenieBastie","@HiTech_lexpress","@bcondominas","@Laurie_Z_","@jeanfrancgerard","@MathieuPagura","@BGUYF","@AlainPiffaretti","@AudreyKucinskas","@julienhory","@Pierrefalga","@TiphThuillier","@cdaniez","@LigerBaptiste","@D_Peras","@julie_dlb","@Fatiha_Temmouri","@julian2lamancha","@GaetaneDeljurie","@JulianMattei","@M_Vicuna","@DeBruynOlivier","@Nehed_Jendoubi","@antoine_grenapi","@ColonnaGen","@VictoriaGairin","@Clement_Lacombe","@TVigoureux","@MargauxObriot","@solinedelos","@RocheSabine","@dangerkens","@EdouardDutour","@MDondeyne","@DupuisNathalie1","@bouscarel","@Mathieu2jean","@Sophie_T_J","@laurentcalixte","@patrockwilliams","@PascaleKremer","@AlexJaquin","@LauraIsaaz","@cath_robin","@Del_Gautherin","@Isaduriez","@lucietuile","@AugeyBastien","@mcastagnet","@AminaKalache","@mvaudano","@CParrot","@ombelinetips","@_JoinLion","@BarbolosiRose","@ToiBruno1","@FloraClodic","@xjbdx","@AlexiaEy","@Emjy_STARK","@elcoco01","@rabilebon","@pflovens_","@FabriceFrossard","@MorganeSERRES","@MarjolaineKoch","@edgarsnow","@SRNLF","@CChassigneux","@KerinecMoran","@NassiraELM","@NewsRicard","@Sandreene","@Emilezrt","@Pierre_Do","@Micode","@CColumelli","@DavidAbiker","@ClementBergantz","@benjaminrabier","@celinekallmann","@edwyplenel","@C_Barbier","@JJBourdin_RMC","@LaurenceFerrari","@aslapix","@IsaMillet","@MaximeSwitek","@tomjoubert","@jszanchi","@roqueeva","@XavierBiseul","@florencesantrot","@AntoineCrochet","@freeman59","@MaudeML","@philippe_gulpi","@mathieum76","@kiouari","@imanemoustakir","@BenedicteMallet","@Emilie_Brouze","@antoinebarret","@_nicolasbocquet","@remibuhagiar","@CourretB","@AymericRobert","@miraelmartins","@pmaniere","@jesuisraphk","@David_Ingram","@pcelerier","@technomedia","@Geraldinedauver","@ThierryLabro","@Newsdusud","@nrauline","@gbregeras","@SCouasnonBFM","@actualites_nrv","@dimitrimoulins","@oli_aura","@FabieChiche","@Vincent_Raimblt","@ChristophGreuet","@PAlbuchay","@MarrauddesGrot","@vtalmon","@cedric","@olivierfrigara","@Julien_Jay","@LydiaBerroyer","@Shuua","@datisdaz","@Steuph","@ameliecharnay","@Bruno_LesNums","@LelloucheNico","@CciliaDiQuinzio","@Elodie_C","@SylvRolland","@Kocobe","@FL_Debes","@jdupontcalbo","@GarciaVictor_","@NicoRichaud","@RHoueix","@simottel","@DamienLicata","@annabelle_L","@Lea_Lejeune","@axel_deb","@marin_eben","@ptiberry","@MatthieuDelach","@sandrinecassini","@benjaminferran","@ppgarcia75","@NotPatrick","@ivalerio","@FabienneSchmitt","@alexgoude","@JeromeColombain","@manhack","@Capucine_Cousin","@Fsorel","@oliviertesquet","@marjoriepaillon","@ginades","@PierreTran","@DelphineCuny","@reesmarc","@lauratenoudji","@ldupin","@carolinedescham","@Lucile_Quillet","@cgabizon","@Allocab","@epenser","@JAGventeprivee","@frwrds","@Laure__Bourdon","@Xavier75","@maximeverner","@s_jourdain","@LoriHelloc"]
 
 
-# pour laisser l'utilisateur choisir ce qu'il veut voir
+#intermediary methods-----------------
+#-----to let the user choose what she wants
 def user_interface
   puts "Bonjour, ceci est un programme qui analyse\nune liste d'username Twitter des journalistes."
   puts "Quelle donnée vous intéresse-elle ？"
   puts "------------"
   puts "Tapez un des choix proposés dessous"
-  puts "---1v. Le nombre de journalistes dans la liste"
+  puts "---1. Le nombre de journalistes dans la liste"
   puts "---2. Le nombre d'usernames contenant un numéro"
   puts "---3. Le nombre d'usernames contenant la combinaison des lettres 'aude' à la suite"
   puts "---4. Le nombre d'usernames commençant par une majuscule"
   puts "---5. Le nombre d'usernames contenant une et une seule majuscule"
   puts "---6. Le nombre d'underscore dans cette liste"
   puts "---7. La position de @epenser dans cette liste"
-  puts "---8v. Cette liste triée par ordre alphabétique A-Z"
+  puts "---8. Cette liste triée par ordre alphabétique A-Z"
   puts "---9. Les 50 usernames les plus courts de cette liste"
-  puts "---0v. Pour terminer le programme"
-  puts "---Lv ou l pour revoir la liste de choix"
+  puts "---0. Pour terminer le programme"
+  puts "---L ou l pour revoir la liste de choix"
+  puts "---T ou t pour tout voir."
   puts "Tapez votre réponse :"
   puts ">>  "
   return gets.chomp
 end
 
-# pour compter le nombre de journalites
+#-----to see whether a name has one and only one cap
+def has_one_cap?(str)
+  i = 0
+  str.each_char do |char|
+     if char == char.upcase
+       i += 1
+     end
+     return i == 1
+  end
+end
+#--------------methodes that really answers those questions-----
+
+
+# 1.pour compter le nombre de journalites
 def _1_count_jour
   puts "La liste contient #{$list.length} journalistes."
 end
 
-# pour comptere le nombre d'underscore
+# 2.pour compter le nb d'username avec chiffre
+def _2_count_num
+  i=0
+  $list.each do |item|
+    if item.count("0-9") > 0
+      i+=1
+    end
+  end
+  "Cette liste contient #{i} usernames ayant au moins un chiffre."
+end
+
+# 3.count usernames having 'aude' lettre combo, case insensitive
+def _3_count_aude
+  i = 0
+  $list.each do |item|
+    if item.downcase.include? "aude"
+      i+=1
+    end
+  end
+  "Cette liste contient #{i} usernames ayant la conbinaison 'aude' min ou maj."
+end
+
+# 4.count usernames that starts with cap letter
+def _4_count_start_cap
+  i=0
+  $list.each do |item|
+    if item[1..-1][0..0] =~ /[A-Z]/
+      i+=1
+    end
+  end
+  "Cette liste contient #{i} usernames commençant par une majucsule."
+end
+# 2 and 4 could have been donw together...
+# too bad I decided to separate them.....
+
+# 5. count usernames with one and only one cap letter
+def _5_count_one_cap
+  i = 0
+  $list.each do |item|
+    if has_one_cap?(item[1..-1])
+      i += 1
+    end
+  end
+  "Cette liste contient #{i} usernames avec seulement une majuscule."
+end
+
+# 6.pour compter le nombre d'underscore
 def _6_count_underscore
   i = 0
   $list.each do |item|
@@ -42,9 +103,31 @@ def _6_count_underscore
 end
 
 
-# pour classer les noms par ordre alphabétique
+# 7.pour la position de @epenser
+def _7_position(username)
+  i = 1
+  $list.each do |item|
+    if item != "@epenser"
+      i += 1
+    else
+      break
+    end
+  end
+  "La position de @epense est #{i}"
+end
+
+# 8.pour classer les noms par ordre alphabétique
 def _8_sort
-  $list.sort
+  print "Voici la liste triée : #{$list.sort}"
+  puts ""
+end
+
+# 9.the 50 shortest
+def _9_50short
+  sort_b_length = $list.sort_by {|x| x.length}
+  puts "Voici les 50 usernames les plus courts"
+  print sort_b_length[0..50]
+  puts""
 end
 
 def run(input)
@@ -52,16 +135,40 @@ def run(input)
     case input
     when '1'
       puts _1_count_jour
-    when '8'
-      print _8_sort
-      puts ""
+    when '2'
+      puts _2_count_num
+    when '3'
+      puts _3_count_aude
+    when '4'
+      puts _4_count_start_cap
+    when '5'
+      puts _5_count_one_cap
     when "6"
       puts _6_count_underscore
+    when "7"
+      puts _7_position("@epenser")
+    when '8'
+      _8_sort
+    when '9'
+      _9_50short
     when '0'
       puts "Au revoir"
       exit!
     when 'l' || 'L'
       user_interface
+    when 't' || 'T'
+      puts _1_count_jour
+      puts _2_count_num
+      puts _3_count_aude
+      puts _4_count_start_cap
+      puts _5_count_one_cap
+      puts _6_count_underscore
+      puts _7_position("@epenser")
+      _8_sort
+      _9_50short
+      puts"-----"
+      puts"Vous avez tout vu. Au revoir"
+      exit!
     end
     puts "--------"
     puts "Voulez vous voir d'autres choses ?\n(L ou l pour revoir les choix)"
